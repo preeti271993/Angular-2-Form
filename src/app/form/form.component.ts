95377611e2f9877de505324f6ad8e8fb44c0fe3c
preeti kumari <pkumari@itechmission.org>
2018-12-16 22:12:37 +0530

    css

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/user.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
 
+export class FormComponent implements OnInit {
+  user: User;
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  resetForm (form? : NgForm){
+  	if(form != null)
+  		form.reset();
+  	    this.user = {
+            UserName='',
+            Password='',
+            Email='',
+            FirstName='',
+            LastName=''
+  	    }
+  }
 }
