ce6256d472d1670bb758f16144b84f6448a3ba4f
pkumari@itechmission.org <preeti2016>
2018-12-31 10:48:02 +0530

    api error resolve

--- src/app/data1.service.ts
+++ src/app/data1.service.ts
@@ -20,13 +20,9 @@ export class Data1Service {
     /*return this.http.request('./data/people.json')
         .map(res => res.json());*/
 
-        return this.http.get('https://jsonplaceholder.typicode.com/users')
+        return this.http.get('https://jsonplaceholder.typicode.com/users');
           // ...and calling .json() on the response to return data
-            .map((res:Response) => res.json())
-          //.map(res => res.json())
-          //...errors if any
-          .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
-          //.catch((error: HttpErrorResponse ) => Observable.throw(error));
+  
 
   } 
 
